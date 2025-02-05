56e00734b59746935c4917387e7576b00abac468
ClaitonSulimann <148346009+ClaitonSulimann@users.noreply.github.com>
2025-02-05 20:45:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+let amigos = [];
+let listaDeNumerosSorteados = [];
+
+function adicionarAmigo() {
+    const nomeInput = document.getElementById("amigo");
+    const listaAmigos = document.getElementById("listaAmigos");
+  
+    if (nomeInput.value) {
+        const novoItem = document.createElement("li");
+        novoItem.textContent = nomeInput.value;
+        listaAmigos.appendChild(novoItem);
+        amigos.push(nomeInput.value);
+        nomeInput.value = "";
+    } else {
+        alert("Por favor, digite um nome!");
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear!");
+        return;
+    }
+
+    let nomeEscolhido = Math.floor(Math.random() * amigos.length);
+
+    while (listaDeNumerosSorteados.includes(nomeEscolhido)) {
+        nomeEscolhido = Math.floor(Math.random() * amigos.length);
+    }
+
+    listaDeNumerosSorteados.push(nomeEscolhido);
+
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.textContent = "O amigo sorteado é: " + amigos[nomeEscolhido];
+}
+
+function removerUltimoAmigo() {
+    if (amigos.length > 0) {
+        amigos.pop();
+        const listaAmigos = document.getElementById("listaAmigos");
+        listaAmigos.removeChild(listaAmigos.lastElementChild);
+    } else {
+        alert("Não há amigos para remover!");
+    }
+}
